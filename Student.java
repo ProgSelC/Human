b9cbf551fd4e41d761a1b7329551f1ec2dbaad88
ProgSelC <ProgSelC@users.noreply.github.com>
2015-07-28 15:20:55 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,7 +2,7 @@ package selcprj;
 
 public class Student extends Human {
 	private Group group;
-
+	private int recbookNum;
 	public Student(String name, String sirname, int age, char gender) {
 		super(name, sirname, age, gender);
 	}
@@ -12,13 +12,21 @@ public class Student extends Human {
 	}
 
 	public String getGroup() {
-		return group.groupNum;
+		return group.getGroupNum();
+	}
+	
+	public void setRecbookNum(int rbNum){
+		this.recbookNum = rbNum;
+	}
+	
+	public int getRecbookNum(){
+		return this.recbookNum;
 	}
 
 	@Override
 	public String toString() {
-		String grNum = (group == null) ? "No group" : group.groupNum;
-		return super.toString() + " group:" + grNum;
+		String grNum = (group == null) ? "No group" : group.getGroupNum();
+		return super.toString() + " group:" + grNum + " recbook:" + recbookNum;
 	}
 
 }
