65c4c60655eb961fa7e8b648afeecd2856230381
ProgSelC <ProgSelC@users.noreply.github.com>
2015-07-28 14:44:43 +0300

    Create GroupIsFullException.java

---
+++ GroupIsFullException.java
@@ -0,0 +1,11 @@
+package selcprj;
+
+public class GroupIsFullException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getMessage(){
+		return "The group is full!";
+	}
+}
